a95bfe7a0688da143764a0b0f5ad69b162952cf7
Павел Хрипунов <khripunovpp@gmail.com>
2019-10-21 14:14:59 +0300

    add mobile choise section

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+var Util = {
+    randomInteger: function(min, max) {
+        var rand = min + Math.random() * (max - min)
+        rand = Math.round(rand);
+        return rand;
+    },
+    scrollToEl: function(el, offset) {
+        $("html,body").animate({ scrollTop: el.offset().top + (offset || 0) }, 500);
+    },
+    trimString: function(string) {
+        return string.split(' ').join('');
+    }
+}
+
+$(function() {
+
+});
